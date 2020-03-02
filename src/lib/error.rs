67edfc379874ba3ee4c72066859267195445e0ba
David Armstrong Lewis <david@armstronglewis.com>
2020-03-01 18:31:21 -0800

    Replace failure with thiserror

--- src/lib/error.rs
+++ src/lib/error.rs
@@ -1,46 +1,42 @@
-use failure::Fail;
 use std::{env, io, path::PathBuf};
+use thiserror::Error;
 
 use serde_yaml::Error as YamlError;
 
-#[derive(Debug, Fail)]
+#[derive(Debug, Error)]
 pub enum Error {
-    #[fail(
-        display = "Looks like your $SHELL environment variable isn't set properly"
-    )]
+    #[error("Looks like your $SHELL environment variable isn't set properly")]
     EnvError(env::VarError),
 
-    #[fail(
-        display = "The config file you specified doesn't exist or isn't valid unicode"
+    #[error(
+        "The config file you specified doesn't exist or isn't valid unicode"
     )]
     IOError(io::Error),
 
-    #[fail(display = "The config file you specified isn't valid YAML")]
+    #[error("The config file you specified isn't valid YAML")]
     SerdeYamlError(YamlError),
 
-    #[fail(display = "The <SHELL> argument provided to --shell is invalid")]
+    #[error("The <SHELL> argument provided to --shell is invalid")]
     BadShellVar(io::Error),
 
-    #[fail(display = "Couldn't find a YAML file to load")]
+    #[error("Couldn't find a YAML file to load")]
     DingusFileNotFound,
 
-    #[fail(
-        display = "The default config path of `~/.config/dingus` doesn't exist"
-    )]
+    #[error("The default config path of `~/.config/dingus` doesn't exist")]
     ConfigPathNotFound,
 
-    #[fail(
-        display = "Found two conflicting config files, specify the file extension or consider renaming them:
+    #[error(
+        "Found two conflicting config files, specify the file extension or consider renaming them:
 {:?}
 {:?}",
         one, two
     )]
     ConflictingConfigPaths { one: PathBuf, two: PathBuf },
 
-    #[fail(display = "No config files provided to load")]
+    #[error("No config files provided to load")]
     EmptyConfigList,
 
-    #[fail(display = "No command was supplied to `run` command")]
+    #[error("No command was supplied to `run` command")]
     NoCommandSupplied,
 }
 
